1a716c62c4d1888a1d59d02f80fb1683f30060db
Puskás Zsolt <errotan@gmail.com>
2019-02-17 14:24:35 +0100

    add jquery, bootstrap, react

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../css/app.css';
+
+import $ from 'jquery';
+import 'bootstrap';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Main from './Main';
+
+ReactDOM.render(<Main />, document.getElementById('main'));
